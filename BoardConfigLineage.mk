a4b67a74346807b6f28d6c0e449c747e27ba9f8a
razorloves <razorloves@gmail.com>
2019-05-25 23:41:28 -0500

    redbull: Build the vendor image
    * Blobs that can be built from source code are built from source, except
      librmnetctl.
    * Various lib and folder symlinks need to be created.
    
    Co-authored-by: Alessandro Astone <ales.astone@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Christopher N. Hesse <raymanfx@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I6e77335985526162433b1b879775a6fd89163c5e

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
@@ -21,6 +23,12 @@ KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/redbull/modules.loa
 KERNEL_MODULES_LOAD := $(foreach m,$(KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(filter-out $(BOOT_KERNEL_MODULES), $(KERNEL_MODULES_LOAD))
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(filter $(BOOT_KERNEL_MODULES), $(KERNEL_MODULES_LOAD))
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor
+
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
