57294618158d063b08721609a581d0b35617c4fb
razorloves <razorloves@gmail.com>
2019-05-25 23:41:28 -0500

    redbull: Build the vendor image
    * Blobs that can be built from source code are built from source, except
      librmnetctl.
    * Various lib and folder symlinks need to be created.
    
    Co-authored-by: Alessandro Astone <ales.astone@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Christopher N. Hesse <raymanfx@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I6e77335985526162433b1b879775a6fd89163c5e

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,3 +15,200 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
+
+# Build necessary packages for system_ext
+
+# Display
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.2 \
+    vendor.display.config@1.3 \
+    vendor.display.config@1.4 \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.6 \
+    vendor.display.config@1.7 \
+    vendor.display.config@1.8 \
+    vendor.display.config@2.0 \
+    vendor.qti.hardware.systemhelper@1.0
+
+# Build necessary packages for vendor
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
+    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor \
+    hardware.google.bluetooth.sar@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+
+# CHRE
+PRODUCT_PACKAGES += \
+    chre
+
+# Codec2
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    libavservices_minijail.vendor \
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_vndk.vendor \
+    libmedia_ecoservice.vendor \
+    libstagefright_bufferpool@2.0.1.vendor
+
+# Confirmation UI
+PRODUCT_PACKAGES += \
+    android.hardware.confirmationui@1.0.vendor:64 \
+    libteeui_hal_support.vendor:64
+
+# Display
+PRODUCT_PACKAGES += \
+    com.google.hardware.pixel.display-V1-ndk.vendor:64 \
+    libdisplayconfig.qti \
+    libqdutils:32 \
+    libqservice:32 \ \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor:64 \
+    vendor.display.config@1.2.vendor:64 \
+    vendor.display.config@1.3.vendor:64 \
+    vendor.display.config@1.4.vendor:64 \
+    vendor.display.config@1.5.vendor:64 \
+    vendor.display.config@1.6.vendor:64 \
+    vendor.display.config@1.7.vendor:64 \
+    vendor.display.config@1.8.vendor:64 \
+    vendor.display.config@1.9.vendor:64 \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
+
+# GNSS
+PRODUCT_PACKAGES += \
+    flp.conf
+
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
+# Identity credential
+PRODUCT_PACKAGES += \
+    android.hardware.identity-V5-ndk.vendor:64 \
+    android.hardware.identity-support-lib.vendor:64 \
+    android.hardware.identity_credential.xml
+
+# Json
+PRODUCT_PACKAGES += \
+    libjson
+
+# Nos
+PRODUCT_PACKAGES += \
+    libnos:64 \
+    libnosprotos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnos_feature:64 \
+    libnos_transport:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64
+
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor:32 \
+    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0.vendor:32 \
+    android.hardware.sensors@2.1.vendor:32
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
+
+# Sensor listener
+PRODUCT_PACKAGES += \
+    lib_sensor_listener:64
+
+# Tether offload
+PRODUCT_PACKAGES += \
+    android.hardware.tetheroffload.config@1.0.vendor:64 \
+    android.hardware.tetheroffload.control@1.0.vendor:64 \
+    libnetfilter_conntrack:64 \
+    libnfnetlink:64
+
+# Tinycompress
+PRODUCT_PACKAGES += \
+    libtinycompress
+
+# VNDK FWK detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti.vendor
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libwifi-hal:64 \
+    libwifi-hal-qcom
+
+# Misc interfaces
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.frameworks.stats@1.0.vendor:64 \
+    android.hardware.authsecret-V1-ndk.vendor:64 \
+    android.hardware.authsecret@1.0.vendor \
+    android.hardware.biometrics.fingerprint@2.1.vendor:64 \
+    android.hardware.biometrics.fingerprint@2.2.vendor:64 \
+    android.hardware.camera.common@1.0.vendor:64 \
+    android.hardware.camera.device@1.0.vendor:64 \
+    android.hardware.camera.device@3.2.vendor:64 \
+    android.hardware.camera.provider@2.4.vendor:64 \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.input.common-V1-ndk.vendor:64 \
+    android.hardware.input.processor-V1-ndk.vendor:64 \
+    android.hardware.keymaster-V3-ndk.vendor:64 \
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.neuralnetworks@1.0.vendor:64 \
+    android.hardware.neuralnetworks@1.1.vendor:64 \
+    android.hardware.neuralnetworks@1.2.vendor:64 \
+    android.hardware.neuralnetworks@1.3.vendor:64 \
+    android.hardware.oemlock-V1-ndk.vendor:64 \
+    android.hardware.oemlock@1.0.vendor:64 \
+    android.hardware.power-V1-ndk.vendor \
+    android.hardware.radio.config@1.0.vendor:64 \
+    android.hardware.radio.config@1.1.vendor:64 \
+    android.hardware.radio.config@1.2.vendor:64 \
+    android.hardware.radio.deprecated@1.0.vendor:64 \
+    android.hardware.radio@1.2.vendor:64 \
+    android.hardware.radio@1.3.vendor:64 \
+    android.hardware.radio@1.4.vendor:64 \
+    android.hardware.radio@1.5.vendor:64 \
+    android.hardware.rebootescrow-V1-ndk.vendor:64 \
+    android.hardware.secure_element@1.1.vendor:64 \
+    android.hardware.secure_element@1.2.vendor:64 \
+    android.hardware.security.keymint-V3-ndk.vendor:64 \
+    android.hardware.security.rkp-V3-ndk.vendor:64 \
+    android.hardware.thermal@1.0.vendor:32 \
+    android.hardware.thermal@2.0.vendor:32 \
+    android.hardware.weaver-V2-ndk.vendor:64 \
+    android.hardware.weaver@1.0.vendor:64 \
+    android.hardware.wifi-V1-ndk.vendor:64 \
+    android.hardware.wifi@1.0.vendor:64 \
+    android.hidl.memory.block@1.0.vendor \
+    android.system.net.netd@1.1.vendor:64 \
+    vendor.qti.hardware.camera.postproc@1.0.vendor:64 \
+    vendor.qti.hardware.systemhelper@1.0.vendor
+
+# Properties
+TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
