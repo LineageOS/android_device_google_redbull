461c2359153819ed14323d030eb8b48b2e87ce8f
razorloves <razorloves@gmail.com>
2020-05-12 23:50:50 -0500

    redbull: Set slotId# of non-removable esim
    This is needed for esim to function properly.
    Without it the EID value could not be read.
    
    Change-Id: I414a7c98362523c5880cfa90b6ce9c0ce92e9c32

--- device-lineage.mk
+++ device-lineage.mk
@@ -83,6 +83,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
