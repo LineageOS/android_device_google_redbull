202c76e22b5728d5a0c20330791b1444330e253e
Eamon Powell <eamonpowell@outlook.com>
2020-12-29 10:16:40 +1100

    redbull: Add missing properties and packages
    Now that we are building our own vendor image, some necessary
    packages aren't being built and some props aren't being set.
    Additionally add missing components from system_ext.
    
    Change-Id: I3e91822b336761cdecf3f6ebe416276dc0585785

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,9 +14,89 @@
 # limitations under the License.
 #
 
+# Build necessary packages for system_ext
+PRODUCT_PACKAGES += \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
+    vendor.display.config@1.8 \
+    vendor.qti.hardware.cryptfshw@1.0
+
+# Build necessary packages for vendor
+PRODUCT_PACKAGES += \
+    android.hardware.identity-support-lib.vendor:64 \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor:32 \
+    android.hardware.sensors@2.0-service.multihal \
+    chre \
+    flp.conf \
+    libavservices_minijail.vendor:64 \
+    libcodec2_hidl@1.0.vendor:64 \
+    libcodec2_vndk.vendor \
+    libcppbor.vendor:64 \
+    libdisplayconfig.qti.vendor:32 \
+    libhidltransport.product \
+    libhwbinder.vendor \
+    libjson \
+    libkeymaster_messages.vendor:64 \
+    libmedia_ecoservice.vendor \
+    libnetfilter_conntrack:64 \
+    libnfnetlink:64 \
+    libnos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnosprotos:64 \
+    libnos_transport:64 \
+    libprotobuf-cpp-full-vendorcompat \
+    libpuresoftkeymasterdevice.vendor:64 \
+    libqdutils:32 \
+    libqservice:32 \
+    libqti_vndfwk_detect.vendor:32 \
+    libsoft_attestation_cert.vendor:64 \
+    libstagefright_bufferpool@2.0.1 \
+    libteeui_hal_support.vendor:64 \
+    libtinycompress \
+    libtinyxml \
+    libvndfwk_detect_jni.qti.vendor \
+    libwifi-hal:64 \
+    libwifi-hal-qcom \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor \
+    vendor.display.config@1.4.vendor:64 \
+    vendor.display.config@1.5.vendor:64 \
+    vendor.display.config@1.6.vendor:64 \
+    vendor.display.config@1.7.vendor:64 \
+    vendor.display.config@1.8.vendor:64 \
+    vendor.display.config@1.9.vendor:64 \
+    vendor.display.config@2.0.vendor:32 \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor:64 \
+    vendor.qti.hardware.cryptfshw@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.0.vendor:64 \
+    vendor.qti.hardware.systemhelper@1.0.vendor \
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
+
+# Properties
+TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
