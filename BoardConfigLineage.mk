49a21b979ed412298c1c2bfcb282976b418faeb0
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-01 22:48:12 +0100

    redbull: Inline kernel building
    Downloaded the vintf modules.load from:
    https://android.googlesource.com/device/google/redbull-kernel/+/refs/tags/android-12.0.0_r14/vintf/modules.load
    
    Change-Id: I1cc3ab1ff45a18d686107a7495b80777b769820f

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,3 +3,23 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := redbull_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/redbull
+TARGET_NEEDS_DTBOIMAGE := true
+# Use a gcc toolchain that offers the aarch64-linux-gnu- prefix
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gas/$(HOST_PREBUILT_TAG)
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+
+# Kernel modules
+KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat device/google/redbull/modules.load))
+KERNEL_MODULES_LOAD := $(foreach m,$(KERNEL_MODULES_LOAD_RAW),$(notdir $(m)))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(filter-out $(BOOT_KERNEL_MODULES), $(KERNEL_MODULES_LOAD))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(filter $(BOOT_KERNEL_MODULES), $(KERNEL_MODULES_LOAD))
