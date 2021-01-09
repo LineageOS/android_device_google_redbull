3207a248466947aa591ee702d756442596b8894d
Alessandro Astone <ales.astone@gmail.com>
2021-01-09 11:28:25 +0100

    redbull: Drop debug boot modules
    Our inline kernel builder has error checking in place, so drop
    debugging modules that the defconfig is not producing
    
    Change-Id: I14dd9a6968655ae755671ff5646c0a3e76362557

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -179,7 +179,6 @@ BOOT_KERNEL_MODULES := \
 	subsystem_notif.ko \
 	ramdump.ko \
 	msm_memshare.ko \
-	msm_bus_dbg_rpmh.ko \
 	msm_bus_rpmh.ko \
 	rpmh-regulator.ko \
 	qcom-geni-se.ko \
@@ -194,7 +193,6 @@ BOOT_KERNEL_MODULES := \
 	spi-geni-qcom.ko \
 	i2c-qcom-geni.ko \
 	ion-alloc.ko \
-	msm_rtb.ko \
 	pinctrl-spmi-gpio.ko \
 	pinctrl-spmi-mpp.ko \
 	pwm-qti-lpg.ko \
@@ -342,7 +340,6 @@ BOOT_KERNEL_MODULES := \
 	leds-qpnp-flash-common.ko \
 	leds-qpnp-flash-v2.ko \
 	of_slimbus.ko \
-	tz_log.ko \
 	google_wlan_mac.ko \
 	msm_ext_display.ko \
 	msm_qmp.ko \
