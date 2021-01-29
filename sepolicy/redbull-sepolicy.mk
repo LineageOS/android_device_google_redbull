a011b654d36a95230b8e0877581ba9b0a8fdf653
Alessandro Astone <ales.astone@gmail.com>
2021-01-30 00:55:01 +0100

    redbull: Build pixel touch hal
    Change-Id: I45cff81e1159613094440dd7b2b73b53519c9de6

--- sepolicy/redbull-sepolicy.mk
+++ sepolicy/redbull-sepolicy.mk
@@ -8,6 +8,7 @@ BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/qcom/sm7250
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/tracking_denials
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/st
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/verizon
+BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/lineage
 
 # Pixel-wide sepolicy
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_sniffer
