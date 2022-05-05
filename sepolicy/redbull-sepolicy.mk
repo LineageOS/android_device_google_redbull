210f3f9d1d51325a7cfca5a89b148ed0f54d2b71
Alessandro Astone <ales.astone@gmail.com>
2022-05-05 16:38:47 +0200

    redbull: sepolicy: Add sepolicy for twoshay
    This allows /vendor/bin/twoshay to start and enables 'Deep Touch'.
    
    Also gets rid of this logspam:
    event_matcher.cc:44 Event queue exceeds maximum size; dropping oldest
    
    Change-Id: I2ab47057fb2ba582ecf680a4d9f2c1cd7faaac42

--- sepolicy/redbull-sepolicy.mk
+++ sepolicy/redbull-sepolicy.mk
@@ -12,6 +12,7 @@ BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/verizon
 # Pixel-wide sepolicy
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_sniffer
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
 
 # system_ext
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/redbull/sepolicy/system_ext/private
