179b3800b1e894f69f76fe245bf40baae3f94fed
Michael Bestas <mkbestas@gmail.com>
2022-08-02 23:38:33 +0300

    Include missing vendor sepolicy
    Change-Id: Id35fb4b22e2ee8dfafdfb8ae601ab823435d2b94

--- sepolicy/redbull-sepolicy.mk
+++ sepolicy/redbull-sepolicy.mk
@@ -1,6 +1,18 @@
 PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/redbull/sepolicy/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/redbull/sepolicy/private
 
+# confirmationui
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/confirmationui_hal
+
+# ramdump
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/ramdump
+
+# twoshay
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
+
+# google_battery service
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+
 # vendors
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/google
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/qcom/common
