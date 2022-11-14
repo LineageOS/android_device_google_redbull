492583acc29f308cc3413c05ac3eac947cd2f773
Michael Bestas <mkbestas@lineageos.org>
2022-11-14 13:11:09 +0200

    redbull: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -11,6 +11,7 @@ TARGET_KERNEL_CONFIG := redbull_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/redbull
 TARGET_NEEDS_DTBOIMAGE := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_NO_GCC := true
 
 # Kernel modules
 BOOT_KERNEL_MODULES += \
