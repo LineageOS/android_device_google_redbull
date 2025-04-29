decaf1b45a19174e99d024c3e719c51114034379
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    redbull: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- lineage_common.mk
+++ lineage_common.mk
@@ -6,3 +6,7 @@
 
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
+
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/sm7250_bms/charge_disable)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
