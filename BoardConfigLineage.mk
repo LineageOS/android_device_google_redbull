5122eae200d6735890eb12fcab8bb0f05533857a
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 09:03:01 +0300

    redbull: Drop no longer used TARGET_KERNEL_NO_GCC
    See: I419ffd8c4f055ca29a6554983ed44aaaab54a666
    Change-Id: I9ce189a991bddd4ec9e42f6853fe5fcd12b51038

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -10,7 +10,6 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := redbull_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/redbull
 TARGET_NEEDS_DTBOIMAGE := true
-TARGET_KERNEL_NO_GCC := true
 
 # Kernel modules
 BOOT_KERNEL_MODULES += \
